e6419be1fd1e121411481e42939c557df145a4dc
Patrick Lutz <patrick.lutz1992@gmail.com>
2015-09-16 00:12:36 +0200

    ammend last commit

---
+++ code/BaServer/src/CommandTask.java
@@ -0,0 +1,48 @@
+import java.util.Timer;
+import java.util.TimerTask;
+
+import connection.UDPConnectionHandler;
+import message.RobotCommand;
+
+
+public class CommandTask extends TimerTask {
+	
+	private static final int VALIDITY = 500;
+	
+	private int sendRate = 100;
+	
+	private volatile RobotCommand bufferedCommand;
+	
+	private Timer timer = new Timer(true);
+	
+	private int stopCount = 0;
+	
+	private UDPConnectionHandler robot;
+	
+	public CommandTask(UDPConnectionHandler robot) {
+		bufferedCommand = new RobotCommand();
+		this.robot = robot;
+	}
+	
+	
+	public void setCommand(RobotCommand command) {
+		bufferedCommand = command;
+		stopCount = 0;
+	}
+
+
+	@Override
+	public void run() {
+		
+		robot.send(bufferedCommand.getBytes());
+		
+		stopCount++;
+		if (stopCount > (VALIDITY / sendRate)) {
+			bufferedCommand = new RobotCommand();
+		}
+	}
+	
+	public void start() {
+		timer.scheduleAtFixedRate(this, 0, sendRate);
+	}
+}
---
+++ code/BaServer/src/Player.java
@@ -0,0 +1,82 @@
+import message.ControllerCommand;
+import message.RobotCommand;
+
+import org.json.simple.JSONObject;
+
+import connection.OnControllerMessageReceived;
+import connection.OnRobotMessageReceived;
+import connection.UDPConnectionHandler;
+import connection.WebsocketSocket;
+
+
+public class Player {
+
+	
+	private WebsocketSocket controller;
+	
+	private UDPConnectionHandler robot;
+	
+	private int score = 0;
+	
+	private CommandTask commandTask;
+	
+	public Player() {
+
+		
+	}
+	
+	
+	public void setDevices(UDPConnectionHandler robot, WebsocketSocket controller) {
+		this.controller = controller;
+		this.robot = robot;
+		
+		controller.registerMessageListener(messageListenerController);						
+		robot.registerMessageListener(messageListenerRobot);
+		
+		commandTask = new CommandTask(robot);
+	}
+	
+	
+	public void unsetDevices() {
+		
+		controller.unregisterMessageListener();
+		robot.unregisterMessageListener();
+		
+		controller = null;
+		robot = null;
+		
+		commandTask.cancel();
+	}
+
+	
+	public void goalDetected() {
+		score++;
+	}
+	
+	
+	public void setScore(int score) {
+		this.score = score;
+	}
+	
+	public int getScore() {
+		return score;
+	}
+	
+	OnControllerMessageReceived messageListenerController = new OnControllerMessageReceived() {
+		
+		@Override
+		public void messageReceived(WebsocketSocket controller, ControllerCommand command) {
+
+			commandTask.setCommand(new RobotCommand(command));
+		}
+	};
+	
+	OnRobotMessageReceived messageListenerRobot = new OnRobotMessageReceived() {
+		
+		@Override
+		public void messageReceived(UDPConnectionHandler robot, byte[] data) {
+
+		
+		}
+	};
+}
